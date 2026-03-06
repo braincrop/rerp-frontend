8dd13e2d613d3c732cfd9fc5737815fb330bee90
Syed-Burhan-Uddin <syed.burhanuddin@braincrop.net>
2026-03-06 12:29:19 +0500

    fix/change-url

--- src/api/SaleLogs/SaleLogHelperApi.js
+++ src/api/SaleLogs/SaleLogHelperApi.js
@@ -3,7 +3,7 @@ import { axiosLocal,axiosInstance } from '../axiosConfig'
 export const GetSaleLogs = async (data) => {
     console.log('data---', data)
   try {
-    const response = await axiosLocal.get('SalesLog/sales-logs',{
+    const response = await axiosInstance.get('SalesLog/sales-logs',{
       params: {
         ...data,
       },
